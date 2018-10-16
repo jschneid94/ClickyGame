57d43d012791e7963832e894c41ff2ac7ca4f24d
SchneiderMan <jschneid94@gmail.com>
2018-10-15 21:00:41 -0700

    Added wrapper, title, and charactercard components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
-import { CharacterCard, Title } from './components';
+import CharacterCard from './components/CharacterCard';
+import Title from './components/Title';
+import Wrapper from './components/Wrapper';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <Title>Clicky Game</Title>
-      <div>Test</div>
-      <CharacterCard />
+      <Wrapper>
+        <Title>Clicky Game</Title>
+        <div>Testy test</div>
+        <CharacterCard />
+      </Wrapper>
     );
   }
 }
---
+++ src/components/CharacterCard/CharacterCard.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./CharacterCard.css";
+
+const CharacterCard = props => (
+    <div className="card">
+        Test
+    </div>
+);
+
+export default CharacterCard;
