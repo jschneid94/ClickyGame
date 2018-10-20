3e323ee5740fb9863a28014e90333b7b1b77c6ca
SchneiderMan <jschneid94@gmail.com>
2018-10-20 10:18:09 -0700

    Updates

---
+++ precache-manifest.7034afbb97fd4fa7f36caf0055343ec4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "59843ae1c2ed89599fd0",
+    "url": "/ClickyGame/static/js/runtime~main.59843ae1.js"
+  },
+  {
+    "revision": "def6bcd43eeecc2bedae",
+    "url": "/ClickyGame/static/js/main.def6bcd4.chunk.js"
+  },
+  {
+    "revision": "231e549fba2c06736f24",
+    "url": "/ClickyGame/static/js/1.231e549f.chunk.js"
+  },
+  {
+    "revision": "def6bcd43eeecc2bedae",
+    "url": "/ClickyGame/static/css/main.7e4615a4.chunk.css"
+  },
+  {
+    "revision": "f422f04714de3925e86630e640712158",
+    "url": "/ClickyGame/index.html"
+  }
+];
